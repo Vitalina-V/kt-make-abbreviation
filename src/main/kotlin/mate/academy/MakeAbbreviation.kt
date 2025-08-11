acc4428df54d112f578a6a7551529233b1a9816f
Volkivska <vitalina.v@air-dr.com>
2025-08-11 10:38:24 +0300

    makeAbbr impl

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,6 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    val split = source.split(" ")
+    return split.joinToString("") { it[0].uppercase() }
 }
